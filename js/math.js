fad765e4c4ae429b41849795425a1cf1ab187e09
Kishore <kishorekumarsaran2001@gmail.com>
2022-10-23 18:14:45 +0530

    math file added

---
+++ js/math.js
@@ -0,0 +1,9 @@
+let number = 22/ 7;
+//Output: 3.142857142857143
+console.log("Output:"+number);
+//Floor
+console.log("floor"+Math.floor(number));
+//Ceil
+console.log("Ceil:"+Math.ceil(number));
+//Generate Random numbers 
+console.log("Random:"+Math.random());
