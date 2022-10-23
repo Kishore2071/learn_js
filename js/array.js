2acc274e64e016a19e20a0fda9cf09a4fcd81ded
Kishore <kishorekumarsaran2001@gmail.com>
2022-10-23 17:23:06 +0530

    array file added

---
+++ js/array.js
@@ -0,0 +1,9 @@
+let heros = ['Iron man','Thor',1234,'Spider man',"Bat man"];
+
+console.log(heros);
+
+console.log(`we have ${heros.length} superheros`);
+//Its not Quotation 
+//Its Backtick 
+//its is top of tab keyboard key 
+
